e666a13ac2f386b2ad2857189ba4ba3ed6444a58
Mohd Arish <arishmd768@gmail>
2023-11-14 23:45:21 +0530

    task1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <>
+
+     <h2>Expanse Item</h2>
+     <h3>Food Rs.100</h3>
+     <h3>Petrol Rs.400</h3>
+     <h3>Movie Rs.100</h3>
+
+     </>
+  )
+}
+
+export default App
