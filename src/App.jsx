d2632af119736657682da10726af86ddb8a835ad
Mohd Arish <arishmd768@gmail>
2023-11-15 21:14:17 +0530

    task2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import Expanse from './Component/Expanse';
 
 const App = () => {
 
-  let expanse=[
-    {title:"Movie",price:200},
-    {title:"Transportation",price:500},
-    {title:"Travel",price:200},
-    {title:"Food",price:700},
-  ]
+  const [expenses, setExpenses] = useState([
+    { id: 1, title: "Movie", price: 200 },
+    { id: 2, title: "Transportation", price: 500 },
+    { id: 3, title: "Travel", price: 200 },
+    { id: 4, title: "Food", price: 700 },
+  ]);
+
+  const DeleteExpanse = (id) => {
+    // Create a new array excluding the expense with the specified id
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
+    // Update the state with the new array
+    setExpenses(updatedExpenses);
+  };
+
+
   return (
      <>
       {
-        expanse.map(({title,price})=>(
+       expenses.map(({title,price,id})=>(
           <Expanse 
           
           title={title}
           price={price}
+          id={id}
+          DeleteExpanse={DeleteExpanse}
           />
         ))
       }
--- src/Component/Expanse.jsx
+++ src/Component/Expanse.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Expanse = ({title,price}) => {
+const Expanse = ({title,price,DeleteExpanse,id}) => {
   return (
     <>
-
+    
+    <div style={{display:"flex" ,gap:10 ,alignItems:"center",justifyContent:"center",marginTop:"2%" }}>
     <h2>{title}</h2>
-    <h3>{price}</h3>
+    <h2>{price}</h2>
+    <button onClick={()=>DeleteExpanse(id)}>Delete</button>
+    </div>
+   
 
     </>
   )
